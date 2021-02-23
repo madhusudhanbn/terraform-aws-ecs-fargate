9f969cd5070e03a9fd101720aae31cdff6c6967f
Bruno Dias <bruno.robles@gmail.com>
2021-02-22 21:09:34 -0300

    add output task security group id

--- outputs.tf
+++ outputs.tf
@@ -1,6 +1,13 @@
-output task_definition_arn {
+output "task_definition_arn" {
   value       = aws_ecs_task_definition.app.arn
   sensitive   = false
   description = "The ARN of the task definition."
+  depends_on  = []
+}
+
+output "task_security_group_id" {
+  value       = aws_security_group.ecs_tasks.id
+  sensitive   = false
+  description = "description"
   depends_on  = []
 }
